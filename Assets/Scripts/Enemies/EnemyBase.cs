b079b59cf81d95664558cf751f0bda7a57d58bb6
César <cesar.romero.borges@gmail.com>
2025-06-23 17:11:47 -0300

    add basic enemy and its animations

---
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -0,0 +1,89 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using Animation;
+
+namespace Enemy
+{
+    public class EnemyBase : MonoBehaviour
+    {
+
+        public float startLife = 10f;
+
+        [SerializeField] private float _currentLife;
+
+        [Header("Animation")]
+        [SerializeField] private AnimationBase _animationBase;
+
+        [Header("Start Animation")]
+        public float startAnimationDuration = .2f;
+        public Ease startAnimationEase = Ease.OutBack;
+        public bool startWithBornAnimation = true;
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        protected void ResetLife()
+        {
+            _currentLife = startLife;
+        }
+
+        protected virtual void Init()
+        {
+            ResetLife();
+
+            if (startWithBornAnimation)
+            {
+                BornAnimation();
+            }
+        }
+
+        protected virtual void Kill()
+        {
+            OnKill();
+        }
+
+        protected virtual void OnKill()
+        {
+            Destroy(gameObject, 3f);
+            PlayAnimationByTrigger(AnimationType.DEATH);
+        }
+
+        public void OnDamage(float f)
+        {
+            _currentLife -= f;
+
+            if (_currentLife <= 0 )
+            {
+                Kill();
+            }
+        }
+
+        #region ANIMATION
+
+        private void BornAnimation()
+        {
+            transform.DOScale(0, startAnimationDuration).SetEase(startAnimationEase).From();
+        }
+
+        public void PlayAnimationByTrigger(AnimationType animationType)
+        {
+            _animationBase.PlayAnimationByTrigger(animationType);
+        }
+
+        #endregion
+
+        //Debug
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                OnDamage(5f);
+            }
+        }
+    }
+}
